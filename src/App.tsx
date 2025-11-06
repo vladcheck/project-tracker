46a59e8631aaec336149e8f41b9be3c86dd209db
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-11-06 09:36:23 +0300

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Моё React приложение</h1>
+    </div>
+  );
+}
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,10 @@
+export default function Greeting() {
+  const userName = "Вова";
+
+  return (
+    <div className="greeting">
+      <h1>Привет!</h1>
+      <p>Рады видеть вас в нашем приложении.</p>
+    </div>
+  );
+}
