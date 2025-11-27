cc4211cb578e0b0a773759a26b31c7462c23d7a2
vladcheck <valdemarcheck@yandex.ru>
2025-11-27 11:36:12 +0300

    fix: base link

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/project-roadmap/",
+  base: "/",
   build: {
     assetsDir: "./public",
   },
